675eba736c735927edb866d00ac6d01010b9eaa8
unknown <tongnguyennhattien140805@gmail.com>
2026-06-24 19:00:26 +0700

    first commit

---
+++ src/main/java/com/teasound/teasound_api/TeasoundApiApplication.java
@@ -0,0 +1,13 @@
+package com.teasound.teasound_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeasoundApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeasoundApiApplication.class, args);
+	}
+
+}
